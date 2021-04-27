b13c3e639c4e1457c16fb7151a4cb00539f90dd5
Max Zahorskiy <44178626+MaxAnte@users.noreply.github.com>
2021-04-27 09:42:17 +0300

    another branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. Here you can see
-          first commited change.
+          first commited change. Another branch
         </p>
         <a
           className="App-link"
