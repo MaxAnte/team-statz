846fa930a523bfeab8c752febf557be3432eb5f7
MaxAnte <qzzik97@gmail.com>
2021-07-02 12:55:58 +0300

    Added general info section in Add game popup

--- app/src/components/AddGamePopup/AddGamePopup.js
+++ app/src/components/AddGamePopup/AddGamePopup.js
@@ -8,8 +8,40 @@ function AddGamePopup({ closeHandler }) {
   return (
     <div className={styles.popupWrap}>
       <h3 className={styles.popupTitle}>Add Game Stats</h3>
-      <h4 className={styles.popupSubtitle}>General game information</h4>
-      <span>Popup</span>
+      <form className={styles.addGameForm}>
+        <h4 className={styles.popupSubtitle}>General game information</h4>
+        <div className={styles.popupSection}>
+          <div className={styles.genGameInfo}>
+            <span className={styles.genGameInfoNames}>Basketball City</span>
+            <input
+              type="text"
+              maxLength="3"
+              name="ourScore"
+              id="ourScore"
+              className={styles.genGameInfoScore}
+            />
+            <span>:</span>
+            <input
+              type="text"
+              maxLength="3"
+              name="enemyScore"
+              id="enemyScore"
+              className={styles.genGameInfoScore}
+            />
+            <input
+              name="enemyName"
+              id="enemyName"
+              className={styles.genGameInfoNames}
+              placeholder="Enemy Team"
+            />
+          </div>
+        </div>
+        <h4 className={styles.popupSubtitle}>
+          Players that have played that game
+        </h4>
+        <span>Popup</span>
+      </form>
+
       <div className={styles.closeBtn} onClick={() => closeHandler()}>
         <CloseIcon width="20px" heigth="20px" color="black" />
       </div>
