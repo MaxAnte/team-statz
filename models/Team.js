fe92db90fd16e6c05f20f0e1827c75cc7e4b9e61
MaxAnte <qzzik97@gmail.com>
2021-07-06 11:07:49 +0300

    fix in Team model

--- models/Team.js
+++ models/Team.js
@@ -1,7 +1,6 @@
 const { Schema, model, Types } = require("mongoose");
 
 const schema = new Schema({
-  id: { type: Types.ObjectId, required: true, unique: true },
   name: { type: String, required: true, unique: true },
   wins: { type: Number, required: true },
   loses: { type: Number, required: true },
