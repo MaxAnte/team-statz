ab0344216246262a17ece6898af9da610b880761
MaxAnte <qzzik97@gmail.com>
2021-07-13 18:56:49 +0300

    calendar gameCard tweak

--- app/src/components/GameCardCalendar/GameCardCalendar.js
+++ app/src/components/GameCardCalendar/GameCardCalendar.js
@@ -6,17 +6,23 @@ function GameCardCalendar({ game }) {
   return (
     <div className={styles.calendarGameCard}>
       <h4 className={styles.calendarGameName}>vs. {game.enemy}</h4>
-      <div className={styles.calendarGameScore}>
-        <span
-          className={`our ${
-            game.ourScore > game.enemyScore ? styles.win : styles.lose
-          }`}
-        >
-          {game.ourScore}
-        </span>
-        :<span>{game.enemyScore}</span>
-      </div>
-      {/* <div className={styles.calendarGameDate}>{game.date}</div> */}
+      {game.ourScore ? (
+        <div className={styles.calendarGameScore}>
+          <span
+            className={`our ${
+              game.ourScore > game.enemyScore ? styles.win : styles.lose
+            }`}
+          >
+            {game.ourScore}
+          </span>
+          :<span>{game.enemyScore}</span>
+        </div>
+      ) : (
+        <div className={styles.calendarGameDate}>
+          <span>{game.date}</span>
+          <span>{game.time}</span>
+        </div>
+      )}
     </div>
   );
 }
