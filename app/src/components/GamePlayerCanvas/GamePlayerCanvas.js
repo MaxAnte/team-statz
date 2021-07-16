dbd3a18022edec45e0206fef4da9e6f462e33e13
MaxAnte <qzzik97@gmail.com>
2021-07-16 14:06:42 +0300

    little tweak in canvas

--- app/src/components/GamePlayerCanvas/GamePlayerCanvas.js
+++ app/src/components/GamePlayerCanvas/GamePlayerCanvas.js
@@ -6,7 +6,7 @@ function GamePlayerCanvas({
   coordinates = [],
   mode = "edit",
   canvID,
-  handleGetCoords = "",
+  handleGetCoords,
 }) {
   const [newCoords, setNewCoords] = useState([]);
   const [canv, setCanv] = useState(undefined);
@@ -32,15 +32,15 @@ function GamePlayerCanvas({
 
   useEffect(() => {
     const canvas = canvasRef.current;
-    setCanv(canvas);
-    setCanvBound(canvas.getBoundingClientRect());
-    var ctx = canvas.getContext("2d");
-    setContext(ctx);
-
     canvas.style.width = WIDTH + "px";
     canvas.style.height = HEIGHT + "px";
     canvas.width = DPI_WIDTH;
     canvas.height = DPI_HEIGHT;
+    var ctx = canvas.getContext("2d");
+
+    setCanv(canvas);
+    setCanvBound(canvas.getBoundingClientRect());
+    setContext(ctx);
 
     if (!coordinates.length) coordinates = newCoords;
     coordinates.forEach((el) => {
@@ -59,16 +59,14 @@ function GamePlayerCanvas({
 
   const draw = (e) => {
     if (mode !== "view") {
-      // if (!e.target.matches(canvID)) return;
-      var pos = getMousePos(e);
+      const { x, y } = getMousePos(e);
       drawType === true
         ? (context.fillStyle = "#008000")
         : (context.fillStyle = "#ff0000");
-      context.fillRect(pos.x, pos.y, 15, 15);
-      setNewCoords((oldState) => [
-        ...oldState,
-        { x: pos.x, y: pos.y, miss: !drawType },
-      ]);
+      context.fillRect(x, y, 15, 15);
+      x >= 0 && y >= 0
+        ? setNewCoords((oldState) => [...oldState, { x, y, miss: !drawType }])
+        : setNewCoords((oldState) => [...oldState, { x: 0, y: 0, miss: true }]);
       handleGetCoords(newCoords);
     }
   };
