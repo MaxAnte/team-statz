66442404915ab5f6e13c1adc8084e7c43cfc09d7
MaxAnte <qzzik97@gmail.com>
2021-07-30 14:30:21 +0300

    v1.0.7 | deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const config = require("config");
+require("dotenv").config();
 const mongoose = require("mongoose");
 const path = require("path");
 
@@ -21,11 +22,11 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = config.get("port") || 5000;
+const PORT = process.env.PORT || 5000;
 
 async function start() {
   try {
-    await mongoose.connect(config.get("mongoUri"), {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
