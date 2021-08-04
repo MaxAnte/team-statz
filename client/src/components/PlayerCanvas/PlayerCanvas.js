dc0b5fa915dbce206dc93afccbbdda37b652a2e0
MaxAnte <qzzik97@gmail.com>
2021-08-04 20:45:10 +0300

    v1.0.9 | added player 3pt zones

--- client/src/components/PlayerCanvas/PlayerCanvas.js
+++ client/src/components/PlayerCanvas/PlayerCanvas.js
@@ -33,9 +33,43 @@ function PlayerCanvas() {
     ctx.moveTo(0, 0);
     ctx.lineTo(200, 0);
     ctx.lineTo(280, 300);
-    ctx.quadraticCurveTo(55, 375, 40, 600);
-    ctx.lineTo(0, 600);
+    ctx.quadraticCurveTo(75, 375, 43, 555);
+    ctx.lineTo(0, 555);
     ctx.fill();
+
+    ctx.fillStyle = "rgb(255, 51, 51, 0.35)";
+    ctx.beginPath();
+    ctx.moveTo(200, 0);
+    ctx.lineTo(560, 0);
+    ctx.lineTo(480, 300);
+    ctx.quadraticCurveTo(390, 275, 280, 300);
+    ctx.fill();
+
+    ctx.fillStyle = "rgb(255, 255, 0, 0.35)";
+    ctx.beginPath();
+    ctx.moveTo(760, 0);
+    ctx.lineTo(560, 0);
+    ctx.lineTo(480, 300);
+    ctx.quadraticCurveTo(680, 355, 720, 555);
+    ctx.lineTo(760, 555);
+    ctx.fill();
+
+    ctx.fillStyle = "rgb(255, 51, 51, 0.35)";
+    ctx.beginPath();
+    ctx.moveTo(0, 555);
+    ctx.lineTo(43, 555);
+    ctx.lineTo(43, 730);
+    ctx.lineTo(0, 730);
+    ctx.fill();
+
+    ctx.fillStyle = "rgb(255, 51, 51, 0.35)";
+    ctx.beginPath();
+    ctx.moveTo(760, 555);
+    ctx.lineTo(720, 555);
+    ctx.lineTo(720, 730);
+    ctx.lineTo(760, 730);
+    ctx.fill();
+
     ctx.stroke();
   }, []);
 
