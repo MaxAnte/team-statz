570c35527b81464028b401d849b67e3ad880d0ff
MaxAnte <qzzik97@gmail.com>
2021-08-17 12:07:59 +0300

    v.1.0.9 | reworked zones, added detection for spots

--- client/src/components/PlayerCanvas/PlayerCanvas.js
+++ client/src/components/PlayerCanvas/PlayerCanvas.js
@@ -1,5 +1,6 @@
 import React, { useRef, useState, useEffect } from "react";
-import { useTranslation } from "react-i18next";
+
+import { zones } from "./zones";
 
 import styles from "./playerCanvas.module.css";
 
@@ -8,7 +9,6 @@ function PlayerCanvas({ player, games }) {
   const [canv, setCanv] = useState(undefined);
   const [canvBound, setCanvBound] = useState(undefined);
   const [context, setContext] = useState(undefined);
-  const { t } = useTranslation();
 
   const canvasRef = useRef(null);
 
@@ -36,6 +36,7 @@ function PlayerCanvas({ player, games }) {
     canvas.width = DPI_WIDTH;
     canvas.height = DPI_HEIGHT;
     var ctx = canvas.getContext("2d");
+    const zonesPoints = Array(14).fill(0);
 
     setCanv(canvas);
     setCanvBound(canvas.getBoundingClientRect());
@@ -43,189 +44,30 @@ function PlayerCanvas({ player, games }) {
 
     coords &&
       coords.forEach((el) => {
-        ctx.beginPath();
-        if (el.miss) {
-          drawMiss(ctx, el);
-        } else {
-          drawMade(ctx, el);
-        }
-        ctx.lineWidth = 5;
-        ctx.closePath();
-        el.miss ? (ctx.strokeStyle = "#ff0000") : (ctx.strokeStyle = "#00d000");
-        ctx.stroke();
+        zones.forEach((zone, id) => {
+          if (
+            ctx.isPointInPath(zone.path, el.x * MULTIPLIER, el.y * MULTIPLIER)
+          )
+            zonesPoints[id] += 1;
+        });
       });
 
-    drawZones(ctx);
+    drawZones(ctx, zonesPoints);
   }, [coords]);
 
-  const drawMiss = (ctx, element) => {
-    ctx.moveTo(element.x * MULTIPLIER - 10, element.y * MULTIPLIER - 10);
-    ctx.lineTo(element.x * MULTIPLIER + 10, element.y * MULTIPLIER + 10);
-    ctx.moveTo(element.x * MULTIPLIER + 10, element.y * MULTIPLIER - 10);
-    ctx.lineTo(element.x * MULTIPLIER - 10, element.y * MULTIPLIER + 10);
-  };
-  const drawMade = (ctx, element) =>
-    ctx.arc(element.x * MULTIPLIER, element.y * MULTIPLIER, 10, 0, 2 * Math.PI);
-
-  const drawZones = (ctx) => {
-    // remember about that canvas is rotated in css
-    // 3pt right top
+  const drawZones = (ctx, zonesPoints) => {
     ctx.strokeStyle = "#000000";
-    ctx.fillStyle = "rgb(255, 255, 0, 0.15)";
     ctx.lineWidth = 4;
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-    ctx.lineTo(363, 0);
-    ctx.lineTo(464, 88);
-    ctx.quadraticCurveTo(355, 163, 317, 290);
-    ctx.lineTo(0, 208);
-    ctx.fill();
-    ctx.stroke();
+    zones.forEach((zone, id) => {
+      ctx.beginPath();
+      ctx.fillStyle = "rgb(255, 255, 0, 0.15)";
+      ctx.stroke(zone.path);
+      ctx.fill(zone.path);
 
-    // 3pt left top
-    ctx.fillStyle = "rgb(255, 255, 0, 0.15)";
-    ctx.beginPath();
-    ctx.moveTo(0, 800);
-    ctx.lineTo(363, 800);
-    ctx.lineTo(464, 712);
-    ctx.quadraticCurveTo(355, 637, 317, 510);
-    ctx.lineTo(0, 592);
-    ctx.fill();
-    ctx.stroke();
-
-    // // 3pt middle top
-    ctx.fillStyle = "rgb(255, 51, 51, 0.15)";
-    ctx.beginPath();
-    ctx.moveTo(0, 208);
-    ctx.lineTo(0, 592);
-    ctx.lineTo(317, 510);
-    ctx.quadraticCurveTo(285, 390, 317, 290);
-    ctx.fill();
-    ctx.stroke();
-
-    // 3pt right bottom
-    ctx.fillStyle = "rgb(255, 51, 51, 0.15)";
-    ctx.beginPath();
-    ctx.moveTo(748, 0);
-    ctx.lineTo(363, 0);
-    ctx.lineTo(464, 88);
-    ctx.quadraticCurveTo(520, 55, 590, 44);
-    ctx.lineTo(748, 44);
-    ctx.fill();
-    ctx.stroke();
-
-    // 3pt left bottom
-    ctx.fillStyle = "rgb(255, 51, 51, 0.15)";
-    ctx.beginPath();
-    ctx.moveTo(748, 800);
-    ctx.lineTo(363, 800);
-    ctx.lineTo(464, 712);
-    ctx.quadraticCurveTo(520, 745, 590, 756);
-    ctx.lineTo(748, 756);
-    ctx.fill();
-    ctx.stroke();
-
-    // 2p right bottom
-    ctx.fillStyle = "rgb(92, 4, 165, 0.15)";
-    ctx.beginPath();
-    ctx.moveTo(748, 46);
-    ctx.lineTo(748, 175);
-    ctx.lineTo(565, 175);
-    ctx.lineTo(464, 88);
-    ctx.quadraticCurveTo(520, 55, 590, 45);
-    ctx.lineTo(590, 46);
-    ctx.fill();
-    ctx.stroke();
-
-    // 2p left bottom
-    ctx.fillStyle = "rgb(92, 4, 165, 0.15)";
-    ctx.beginPath();
-    ctx.moveTo(748, 756);
-    ctx.lineTo(748, 625);
-    ctx.lineTo(565, 625);
-    ctx.lineTo(464, 712);
-    ctx.quadraticCurveTo(520, 745, 590, 755);
-    ctx.lineTo(590, 756);
-    ctx.fill();
-    ctx.stroke();
-
-    // 2p right top
-    ctx.fillStyle = "rgb(140, 255, 0, 0.15)";
-    ctx.beginPath();
-    ctx.moveTo(565, 175);
-    ctx.lineTo(464, 88);
-    ctx.quadraticCurveTo(355, 163, 317, 290);
-    ctx.lineTo(450, 323);
-    ctx.quadraticCurveTo(470, 220, 570, 175);
-    ctx.fill();
-    ctx.stroke();
-
-    // 2p left top
-    ctx.fillStyle = "rgb(140, 255, 0, 0.15)";
-    ctx.beginPath();
-    ctx.moveTo(565, 625);
-    ctx.lineTo(464, 712);
-    ctx.quadraticCurveTo(355, 637, 317, 510);
-    ctx.lineTo(450, 477);
-    ctx.quadraticCurveTo(470, 580, 570, 625);
-    ctx.fill();
-    ctx.stroke();
-
-    // 2p middle top
-    ctx.fillStyle = "rgb(92, 4, 165, 0.15)";
-    ctx.beginPath();
-    ctx.moveTo(317, 290);
-    ctx.quadraticCurveTo(285, 390, 317, 510);
-    ctx.lineTo(450, 477);
-    ctx.lineTo(450, 323);
-    ctx.fill();
-    ctx.stroke();
-
-    // close right
-    ctx.fillStyle = "rgb(255, 255, 0, 0.15)";
-    ctx.beginPath();
-    ctx.moveTo(748, 175);
-    ctx.lineTo(748, 332);
-    ctx.lineTo(655, 332);
-    ctx.quadraticCurveTo(630, 335, 615, 350);
-    ctx.lineTo(490, 240);
-    ctx.quadraticCurveTo(500, 215, 565, 175);
-    ctx.fill();
-    ctx.stroke();
-
-    // close left
-    ctx.fillStyle = "rgb(255, 255, 0, 0.15)";
-    ctx.beginPath();
-    ctx.moveTo(748, 625);
-    ctx.lineTo(748, 468);
-    ctx.lineTo(655, 468);
-    ctx.quadraticCurveTo(630, 465, 615, 450);
-    ctx.lineTo(488, 562);
-    ctx.quadraticCurveTo(500, 585, 565, 625);
-    ctx.fill();
-    ctx.stroke();
-
-    // close top
-    ctx.fillStyle = "rgb(140, 255, 0, 0.15)";
-    ctx.beginPath();
-    ctx.moveTo(490, 235);
-    ctx.quadraticCurveTo(460, 270, 450, 323);
-    ctx.lineTo(450, 477);
-    ctx.quadraticCurveTo(460, 525, 488, 562);
-    ctx.lineTo(615, 450);
-    ctx.bezierCurveTo(585, 425, 585, 375, 615, 350);
-    ctx.fill();
-    ctx.stroke();
-
-    // center
-    ctx.fillStyle = "rgb(255, 51, 51, 0.15)";
-    ctx.beginPath();
-    ctx.moveTo(748, 332);
-    ctx.lineTo(655, 332);
-    ctx.bezierCurveTo(570, 340, 570, 460, 655, 468);
-    ctx.lineTo(748, 468);
-    ctx.fill();
-    ctx.stroke();
+      ctx.font = "48px serif";
+      ctx.fillStyle = "#000000";
+      ctx.fillText(zonesPoints[id], zone.tPoint.x, zone.tPoint.y);
+    });
   };
 
   return (
---
+++ client/src/components/PlayerCanvas/zones.js
@@ -0,0 +1,56 @@
+// remember about that canvas is rotated in css
+const three_l_b = new Path2D(
+  "M 748 800 L 363 800 L 464 712 Q 520 745 590 756 L 748 756 Z"
+);
+const three_l_t = new Path2D(
+  "M 0 800 L 363 800 L 464 712 Q 355 637 317 510 L 0 592 Z"
+);
+const three_m_t = new Path2D("M 0 208 L 317 290 Q 285 390 317 510 L 0 592 Z");
+const three_r_t = new Path2D(
+  "M 0 0 h 363 L 464 88 Q 355 163 317 290 L 0 208 Z"
+);
+const three_r_b = new Path2D(
+  "M 748 0 L 363 0 L 464 88 Q 520 55 590 44 L 748 44 Z"
+);
+const two_l_b = new Path2D(
+  "M 748 756 L 748 625 L 565 625 L 464 712 Q 520 745 590 755 L 590 756 Z"
+);
+const two_l_t = new Path2D(
+  "M 565 625 L 464 712 Q 355 637 317 510 L 450 477 Q 470 580 570 625 Z"
+);
+const two_m_t = new Path2D("M 317 290 Q 285 390 317 510 L 450 477 L 450 323 Z");
+const two_r_t = new Path2D(
+  "M 565 175 L 464 88 Q 355 163 317 290 L 450 323 Q 470 220 570 175 Z"
+);
+const two_r_b = new Path2D(
+  "M 748 46 L 748 175 L 565 175 L 464 88 Q 520 55 590 45 L 590 46 Z"
+);
+const close_l = new Path2D(
+  "M 748 625 L 748 468 L 655 468 Q 630 465 615 450 L 488 562 Q 500 585 565 625 Z"
+);
+const close_t = new Path2D(
+  "M 488 239 Q 460 270 450 323 L 450 477 Q 460 525 488 562 L 615 450 C 585 430 585 370 615 350 Z"
+);
+const close_r = new Path2D(
+  "M 748 175 L 748 332 L 655 332 Q 630 335 615 350 L 488 239 Q 500 215 565 175 Z"
+);
+const center = new Path2D(
+  "M 748 332 L 655 332 C 570 350 570 450 655 468 L 748 468 Z"
+);
+
+export const zones = [
+  { path: three_l_b, tPoint: { x: 450, y: 780 } },
+  { path: three_l_t, tPoint: { x: 170, y: 680 } },
+  { path: three_m_t, tPoint: { x: 140, y: 400 } },
+  { path: three_r_t, tPoint: { x: 180, y: 130 } },
+  { path: three_r_b, tPoint: { x: 450, y: 50 } },
+  { path: two_l_b, tPoint: { x: 625, y: 705 } },
+  { path: two_l_t, tPoint: { x: 415, y: 610 } },
+  { path: two_m_t, tPoint: { x: 370, y: 420 } },
+  { path: two_r_t, tPoint: { x: 420, y: 215 } },
+  { path: two_r_b, tPoint: { x: 625, y: 125 } },
+  { path: close_l, tPoint: { x: 650, y: 590 } },
+  { path: close_t, tPoint: { x: 480, y: 420 } },
+  { path: close_r, tPoint: { x: 650, y: 235 } },
+  { path: center, tPoint: { x: 610, y: 420 } },
+];
