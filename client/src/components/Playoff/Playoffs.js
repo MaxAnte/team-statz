ea506972c9c9cfb00b2253c059613c190dc8bcb3
MaxAnte <qzzik97@gmail.com>
2021-08-31 15:06:39 +0300

    v1.1.7 | little fix

--- client/src/components/Playoff/Playoffs.js
+++ client/src/components/Playoff/Playoffs.js
@@ -3,7 +3,7 @@ import { useHttp } from "../../hooks/http.hook";
 import { useMessage } from "../../hooks/message.hook";
 import { useTranslation } from "react-i18next";
 
-import styles from "./playoff.module.css";
+import styles from "./playoffs.module.css";
 
 function Playoffs() {
   const { request, error, clearError } = useHttp();
