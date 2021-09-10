bf7239f4b7b585283efb3c592ee6b37f16151bc7
MaxAnte <qzzik97@gmail.com>
2021-09-10 17:53:54 +0300

    v1.2.8 | removed icomoon shit, added navbar icons, added userPanel

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,16 +1,37 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, {
+  useState,
+  useEffect,
+  useCallback,
+  useContext,
+  useRef,
+} from "react";
 import Cookie from "js-cookie";
 import { NavLink } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import { SessionContext } from "../../context/session.provider";
+import { useOutsideClickHandler } from "../../hooks/outsideClick.hook";
 
 import AuthModal from "../AuthModal/AuthModal";
 import Select from "../Select/select";
+import TeamIcon from "../../assets/icons/TeamIcon";
+import ScheduleIcon from "../../assets/icons/ScheduleIcon";
+import StatsIcon from "../../assets/icons/StatsIcon";
+import UserIcon from "../../assets/icons/UserIcon";
+import SettingsIcon from "../../assets/icons/SettingsIcon";
+import LogoutIcon from "../../assets/icons/LogoutIcon";
 
 import styles from "./header.module.css";
 
 function Header() {
+  const { isAuthenticated, logout } = useContext(SessionContext);
   const [modal, setModal] = useState(false);
   const { t, i18n } = useTranslation();
+  const userPanelRef = useRef(null);
+  const closeUserPanel = useOutsideClickHandler(userPanelRef);
+
+  useEffect(() => {
+    if (closeUserPanel && modal) setModal(false);
+  }, [closeUserPanel]);
 
   useEffect(() => {
     const lang = Cookie.get("language");
@@ -35,16 +56,19 @@ function Header() {
         <ul>
           <li>
             <NavLink to="/team" activeClassName={styles.isActive}>
+              <TeamIcon width="36px" heigth="36px" />
               <span>{t("Team")}</span>
             </NavLink>
           </li>
           <li>
             <NavLink to="/schedule" activeClassName={styles.isActive}>
+              <ScheduleIcon width="36px" heigth="36px" />
               <span>{t("Schedule")}</span>
             </NavLink>
           </li>
           <li>
             <NavLink to="/stats" activeClassName={styles.isActive}>
+              <StatsIcon width="36px" heigth="36px" />
               <span>{t("Stats")}</span>
             </NavLink>
           </li>
@@ -58,10 +82,33 @@ function Header() {
         type="language"
         arrow={false}
       />
-      <span className={styles.auth} onClick={() => toggleModal()}>
-        <span>{t("Moder")}</span>
+      <span
+        className={styles.auth}
+        onClick={() => toggleModal()}
+        ref={userPanelRef}
+      >
+        <UserIcon width="36px" height="36px" />
+        {!isAuthenticated && !modal ? <span>{t("Moder")}</span> : null}
+        {isAuthenticated ? (
+          <div
+            className={`${styles.userPanel} ${
+              modal ? styles.userPanelOpened : ""
+            }`}
+          >
+            <NavLink to={"/app/settings"}>
+              <SettingsIcon width="30px" height="30px" />
+            </NavLink>
+            <button
+              type="button"
+              onClick={() => logout()}
+              className={styles.hoverLogout}
+            >
+              <LogoutIcon width="30px" height="30px" />
+            </button>
+          </div>
+        ) : null}
       </span>
-      {modal ? (
+      {modal && !isAuthenticated ? (
         <>
           <AuthModal closeOnLogin={closeOnLogin} />
           <div className={styles.modalBg} onClick={() => toggleModal()}></div>
