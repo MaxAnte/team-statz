65a5bde198ed1cb44958a51e7a35a820e1df4fbc
MaxAnte <qzzik97@gmail.com>
2021-09-10 11:01:17 +0300

    v1.2.6 | migrated get api to appProvider

--- client/src/context/app.provider.js
+++ client/src/context/app.provider.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, createContext } from "react";
+import React, { useState, createContext } from "react";
 import { useHttp } from "../hooks/http.hook";
 import { useMessage } from "../hooks/message.hook";
 
@@ -7,6 +7,157 @@ export const AppContext = createContext(undefined);
 export const AppProvider = ({ children }) => {
   const message = useMessage();
   const { request, clearError } = useHttp();
+
+  const getSettings = async () => {
+    setAppState((prevAppState) => ({
+      ...prevAppState,
+      loading: true,
+    }));
+    try {
+      const response = await request("/api/settings/get", "POST", {});
+      setAppState((prevAppState) => ({
+        ...prevAppState,
+        settings: Object.values(response)[0],
+        loading: false,
+      }));
+      return response;
+    } catch (e) {
+      message(e.message);
+      clearError();
+    }
+  };
+
+  const getTeams = async () => {
+    setAppState((prevAppState) => ({
+      ...prevAppState,
+      loading: true,
+    }));
+    try {
+      const response = await request("/api/team/teams", "POST", {});
+      setAppState((prevAppState) => ({
+        ...prevAppState,
+        teams: Object.values(response),
+        loading: false,
+      }));
+      return response;
+    } catch (e) {
+      message(e.message);
+      clearError();
+    }
+  };
+
+  const getPlayers = async () => {
+    setAppState((prevAppState) => ({
+      ...prevAppState,
+      loading: true,
+    }));
+    try {
+      const response = await request("/api/player/players", "POST", {});
+      if (Object.keys(response).length) {
+        const bestPts = { pts: 0, id: 0 };
+        const bestReb = { reb: 0, id: 0 };
+        const bestAst = { ast: 0, id: 0 };
+        const bestBlk = { blk: 0, id: 0 };
+        const bestStl = { stl: 0, id: 0 };
+        Object.values(response).forEach((el) => {
+          if (el.pts > bestPts.pts) {
+            bestPts.pts = el.pts;
+            bestPts.id = el._id;
+          }
+          if (el.reb > bestReb.reb) {
+            bestReb.reb = el.reb;
+            bestReb.id = el._id;
+          }
+          if (el.ast > bestAst.ast) {
+            bestAst.ast = el.ast;
+            bestAst.id = el._id;
+          }
+          if (el.blk > bestBlk.blk) {
+            bestBlk.blk = el.blk;
+            bestBlk.id = el._id;
+          }
+          if (el.stl > bestStl.stl) {
+            bestStl.stl = el.stl;
+            bestStl.id = el._id;
+          }
+        });
+        Object.values(response).forEach((el) => {
+          if (el._id === bestPts.id) el.bestInPts = true;
+          if (el._id === bestReb.id) el.bestInReb = true;
+          if (el._id === bestAst.id) el.bestInAst = true;
+          if (el._id === bestBlk.id) el.bestInBlk = true;
+          if (el._id === bestStl.id) el.bestInStl = true;
+        });
+      }
+      setAppState((prevAppState) => ({
+        ...prevAppState,
+        players: Object.values(response),
+        loading: false,
+      }));
+      return response;
+    } catch (e) {
+      message(e.message);
+      clearError();
+    }
+  };
+
+  const getGames = async () => {
+    setAppState((prevAppState) => ({
+      ...prevAppState,
+      loading: true,
+    }));
+    try {
+      const response = await request("/api/game/games", "POST", {});
+      setAppState((prevAppState) => ({
+        ...prevAppState,
+        games: response,
+        loading: false,
+      }));
+      return response;
+    } catch (e) {
+      message(e.message);
+      clearError();
+    }
+  };
+
+  const getDates = async () => {
+    setAppState((prevAppState) => ({
+      ...prevAppState,
+      loading: true,
+    }));
+    try {
+      const response = await request("/api/date/dates", "POST", {});
+      setAppState((prevAppState) => ({
+        ...prevAppState,
+        dates: response,
+        loading: false,
+      }));
+      return response;
+    } catch (e) {
+      message(e.message);
+      clearError();
+    }
+  };
+
+  const getPlayoffsMatchups = async () => {
+    setAppState((prevAppState) => ({
+      ...prevAppState,
+      loading: true,
+    }));
+    try {
+      const response = await request("/api/bracket/get", "POST", {});
+      setAppState((prevAppState) => ({
+        ...prevAppState,
+        playoffsmatchups: response,
+        loading: false,
+      }));
+      return response;
+    } catch (e) {
+      message(e.message);
+      clearError();
+    }
+  };
+
   const [appState, setAppState] = useState({
     settings: {},
     teams: [],
@@ -14,19 +165,15 @@ export const AppProvider = ({ children }) => {
     games: [],
     dates: [],
     playoffsmatchups: [],
-  });
 
-  useEffect(() => {
-    (async () => {
-      try {
-        const response = await request("/api/settings/get", "POST", {});
-        setAppState({ ...appState, settings: Object.values(response)[0] });
-      } catch (e) {
-        message(e.message);
-        clearError();
-      }
-    })();
-  }, [request, message, clearError]);
+    getSettings: getSettings,
+    getTeams: getTeams,
+    getPlayers: getPlayers,
+    getGames: getGames,
+    getDates: getDates,
+    getPlayoffsMatchups: getPlayoffsMatchups,
+    loading: false,
+  });
 
   return <AppContext.Provider value={appState}>{children}</AppContext.Provider>;
 };
