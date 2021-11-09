b3bc67fc628cd21ed6762d6f62ebe44c4cdb56fc
MaxAnte <qzzik97@gmail.com>
2021-11-09 11:41:33 +0200

    some fixes on Player, AddDate and Select

--- client/src/components/Player/player.tsx
+++ client/src/components/Player/player.tsx
@@ -14,6 +14,7 @@ import {
 import PlayerCanvas from "../PlayerCanvas/playerCanvas";
 import TableSheet from "../TableSheet/tableSheet";
 
+import blankPhoto from "../../assets/images/players/blank-silhouette.png";
 import HeightIcon from "../../assets/icons/heightIcon";
 import WeightIcon from "../../assets/icons/weightIcon";
 import JerseyIcon from "../../assets/icons/jerseyIcon";
@@ -100,7 +101,7 @@ function Player() {
           <div className={styles.top}>
             <h2 className={styles.nameMob}>{player.name}</h2>
             <div className={styles.image}>
-              <img src={player.image_thumb} alt={player.name} />
+              <img src={player.image_thumb || blankPhoto} alt={player.name} />
             </div>
             <div className={styles.info}>
               <h2 className={styles.name}>{player.name}</h2>
